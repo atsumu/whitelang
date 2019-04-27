c91d7c4bcc20f804efc2e6227058670bae652034
atsumu <atsumu.dev@gmail.com>
2019-04-27 22:59:58 +0900

    fix linefeed

--- test/parse.js
+++ test/parse.js
@@ -4,7 +4,7 @@ const { Parse } = require('../src/parse');
 const { Scan } = require('../src/scan');
 const util = require('util');
 
-var tokens = Scan.top('1 + 1');
+var tokens = Scan.top('1 + 2 + 3');
 var node = Parse.top(tokens);
 console.log(node.show());
 //console.log(util.inspect(node, false, null, true));
